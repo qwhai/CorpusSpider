4f80975f56e901f37aa4296760caa3c87a1ee658
William-Hai <return_zero0@163.com>
2016-01-05 17:55:05 +0800

    update

--- src/org/spider/corpus/tools/ExtractionHTMLText.java
+++ src/org/spider/corpus/tools/ExtractionHTMLText.java
@@ -25,7 +25,7 @@ import org.utils.naga.threads.ThreadUtils;
  * 2015‎年‎12‎月‎3‎日
  * 
  * @author <a href="http://weibo.com/u/5131020927">Q-WHai</a>
- * @see <a href="http://blog.csdn.net/lemon_tree12138">http://blog.csdn.net/lemon_tree12138</a>
+ * @see <a href="http://blog.csdn.net/lemon_tree12138">http://blog.csdn.ne7t/lemon_tree12138</a>
  * @version 0.1
  */
 public class ExtractionHTMLText {
